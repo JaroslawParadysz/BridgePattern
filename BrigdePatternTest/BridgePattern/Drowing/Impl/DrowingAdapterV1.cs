58c80948459d5ace759cfad4b981e4aef6640f5a
Jarek <Jarek@Jarek.com>
2019-08-10 11:39:44 +0200

    refactoring

--- BrigdePatternTest/BridgePattern/Drowing/Impl/DrowingAdapterV1.cs
+++ BrigdePatternTest/BridgePattern/Drowing/Impl/DrowingAdapterV1.cs
@@ -4,21 +4,21 @@ namespace BridgePattern.Drowing.Impl
 {
     public class DrowingAdapterV1 : IDrowingService
     {
-        private readonly DP1 _dp1;
+        private readonly DP1 _dp;
 
         public DrowingAdapterV1()
         {
-            _dp1 = new DP1();
+            _dp = new DP1();
         }
 
         public void DrowACircle(int x, int y, int r)
         {
-            _dp1.DrowACircle(x,y,r);
+            _dp.DrowACircle(x,y,r);
         }
 
         public void DrowALine(int x1, int y1, int x2, int y2)
         {
-            _dp1.DrowALine(x1,y1=x2,y1,y2);
+            _dp.DrowALine(x1,y1=x2,y1,y2);
         }
     }
 }
