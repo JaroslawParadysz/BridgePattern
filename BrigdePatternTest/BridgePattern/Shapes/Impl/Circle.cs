fc3c0f744032596ff371e57d5838b339710053f3
Jarek <Jarek@Jarek.com>
2019-08-10 11:37:56 +0200

    Last changes

--- BrigdePatternTest/BridgePattern/Shapes/Impl/Circle.cs
+++ BrigdePatternTest/BridgePattern/Shapes/Impl/Circle.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BridgePattern.Drowing;
 
 namespace BridgePattern.Shapes.Impl
 {
-    class Circle
+    public class Circle : Shape
     {
+        private int x, y, r;
+        public Circle(IDrowingService drowingService) : base(drowingService)
+        {
+        }
+
+        public override void Drow()
+        {
+            _drowingService.DrowACircle(x, y, r);
+        }
     }
 }
--- BrigdePatternTest/BridgePattern/Shapes/Impl/Rectangular.cs
+++ BrigdePatternTest/BridgePattern/Shapes/Impl/Rectangular.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BridgePattern.Drowing;
 
 namespace BridgePattern.Shapes.Impl
 {
-    class Rectangular
+    public class Rectangular : Shape
     {
+        private int x1, y1, x2, y2;
+        public Rectangular(IDrowingService drowingService) : base(drowingService)
+        {
+        }
+
+        public override void Drow()
+        {
+            _drowingService.DrowALine(x1, y1, x2, y2);
+        }
     }
 }
