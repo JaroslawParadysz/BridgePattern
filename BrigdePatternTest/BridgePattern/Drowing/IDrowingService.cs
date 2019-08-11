cd239bcdebbe051ba147a6c6ffcca7a938658006
Jarek <Jarek@Jarek.com>
2019-08-11 16:07:14 +0200

    Adding commit

--- BrigdePatternTest/BridgePattern/Drowing/IDrowingService.cs
+++ BrigdePatternTest/BridgePattern/Drowing/IDrowingService.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace BridgePattern.Drowing
-{
+{/// <summary>
+/// Comment on master
+/// </summary>
     public interface IDrowingService
     {
         void DrowALine(int x1, int y1, int x2, int y2);
