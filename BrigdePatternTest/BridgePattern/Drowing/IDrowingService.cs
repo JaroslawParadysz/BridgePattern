e46eaaf6f99dbe5346f76bbb43b42304ac3f52c8
Jarek <Jarek@Jarek.com>
2019-08-11 15:15:35 +0200

    Revert "Unwonted change"
    This reverts commit d2d6bc3662705bdab962c1846eeb76d2038ed3c5.

--- BrigdePatternTest/BridgePattern/Drowing/IDrowingService.cs
+++ BrigdePatternTest/BridgePattern/Drowing/IDrowingService.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace BridgePattern.Drowing
 {
-    public interface IDrowingService jarek
+    public interface IDrowingService
     {
         void DrowALine(int x1, int y1, int x2, int y2);
         void DrowACircle(int x, int y, int r);
